01391836bee2ab3fff005f8b18563a967fbc0c2b
Patrick Koers <pkoers75@gmail.com>
2023-07-04 12:34:55 +0200

    multiple reply-to is working, ready for mvp

--- app/mailers/send_mailer.rb
+++ app/mailers/send_mailer.rb
@@ -2,11 +2,11 @@ class SendMailer < ApplicationMailer
   require 'open-uri'
   require 'uri'
 
-  def send_email(user, file_name, file_data, full_subject)
+  def send_email(user, file_name, file_data, full_subject, replyto)
     attachments["#{file_name}.pdf"] = file_data
 
     puts 'Sending email'
-    mail(to: user, from: 'loadsheet@mixty.com', subject: full_subject, reply_to: 'patrick.koers@klm.com,mail.alteafm.database@klm.com') do |format|
+    mail(to: user, from: 'loadsheet@mixty.com', subject: full_subject, reply_to: replyto) do |format|
       format.text { render plain: 'This is an automated e-mail from Altea Flight Management.
         NOT FOR OPERATIONAL USE - TEST
         Please do not reply.
