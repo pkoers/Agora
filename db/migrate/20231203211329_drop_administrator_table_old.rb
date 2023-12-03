de5f78d81d5467b39f9a7609a9b33b25ff25a522
Patrick Koers <pkoers75@gmail.com>
2023-12-03 23:24:05 +0100

    fixed another migration files problem

--- db/migrate/20231203211329_drop_administrator_table_old.rb
+++ db/migrate/20231203211329_drop_administrator_table_old.rb
@@ -1,14 +1,2 @@
 class DropAdministratorTable < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :administrators
-  end
-
-  def down
-    create_table :administrators do |t|
-      t.boolean :trusted
-      t.references :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
 end
